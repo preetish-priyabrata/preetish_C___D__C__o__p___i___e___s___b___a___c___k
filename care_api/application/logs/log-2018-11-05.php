baaba543b5c24029d06122dbf8b74cab54696a47
preetish-priyabrata <ppriyabrata8888@gmail.com>
2019-03-06 11:08:57 +0530

    file care upload by pk

---
+++ care_api/application/logs/log-2018-11-05.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2018-11-05 21:02:57 --> Config Class Initialized
+INFO - 2018-11-05 21:02:57 --> Hooks Class Initialized
+DEBUG - 2018-11-05 21:02:57 --> UTF-8 Support Enabled
+INFO - 2018-11-05 21:02:57 --> Utf8 Class Initialized
+INFO - 2018-11-05 21:02:57 --> URI Class Initialized
+DEBUG - 2018-11-05 21:02:57 --> No URI present. Default controller set.
+INFO - 2018-11-05 21:02:57 --> Router Class Initialized
+INFO - 2018-11-05 21:02:57 --> Output Class Initialized
+INFO - 2018-11-05 21:02:57 --> Security Class Initialized
+DEBUG - 2018-11-05 21:02:57 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-11-05 21:02:57 --> Input Class Initialized
+INFO - 2018-11-05 21:02:57 --> Language Class Initialized
+INFO - 2018-11-05 21:02:57 --> Loader Class Initialized
+INFO - 2018-11-05 21:02:57 --> Helper loaded: url_helper
+INFO - 2018-11-05 21:02:57 --> Helper loaded: file_helper
+INFO - 2018-11-05 21:02:57 --> Controller Class Initialized
+INFO - 2018-11-05 21:02:57 --> File loaded: /var/www/html/cd_copy/care_api/application/views/welcome_message.php
+INFO - 2018-11-05 21:02:57 --> Final output sent to browser
+DEBUG - 2018-11-05 21:02:57 --> Total execution time: 0.0679
---
+++ care_api/application/logs/log-2019-01-11.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2019-01-11 16:55:29 --> Config Class Initialized
+INFO - 2019-01-11 16:55:29 --> Hooks Class Initialized
+DEBUG - 2019-01-11 16:55:29 --> UTF-8 Support Enabled
+INFO - 2019-01-11 16:55:29 --> Utf8 Class Initialized
+INFO - 2019-01-11 16:55:29 --> URI Class Initialized
+DEBUG - 2019-01-11 16:55:29 --> No URI present. Default controller set.
+INFO - 2019-01-11 16:55:29 --> Router Class Initialized
+INFO - 2019-01-11 16:55:29 --> Output Class Initialized
+INFO - 2019-01-11 16:55:29 --> Security Class Initialized
+DEBUG - 2019-01-11 16:55:29 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-01-11 16:55:29 --> Input Class Initialized
+INFO - 2019-01-11 16:55:29 --> Language Class Initialized
+INFO - 2019-01-11 16:55:29 --> Loader Class Initialized
+INFO - 2019-01-11 16:55:29 --> Helper loaded: url_helper
+INFO - 2019-01-11 16:55:29 --> Helper loaded: file_helper
+INFO - 2019-01-11 16:55:29 --> Controller Class Initialized
+INFO - 2019-01-11 16:55:29 --> File loaded: /var/www/html/cd_copy/care_api/application/views/welcome_message.php
+INFO - 2019-01-11 16:55:29 --> Final output sent to browser
+DEBUG - 2019-01-11 16:55:29 --> Total execution time: 0.0802
