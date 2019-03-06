95cd9c9dd39572467285770531df7571814f29cc
preetish-priyabrata <ppriyabrata8888@gmail.com>
2019-03-06 11:04:09 +0530

    file to be transfer

---
+++ alumni/index.php
@@ -0,0 +1,344 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/style2.css">
+   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container-fluid" >
+<!-- <div class="container">
+	<div class="row"> -->
+		<div id="wrapper">
+			<div id="header">
+			<!-- STOP PGNET INDEX -->
+				<p id="loginDL">
+					Welcome | 
+					<a href="/get/page/register?pg=yes&amp;nu=/get/page/home">Register</a> | 
+					<a href="/get/page/login?pg=yes&amp;am=cas&amp;fmt=20&amp;nu=/get/page/home">Log In</a>
+				</p>
+				<!-- end login registration -->
+				<div id="tools" class="clearfix">
+					<!-- <div class="buttons" style="border-right:0;">
+						<a href="/get/page/directory/search?pgOrg=saa"><img src="/content/saa/homepage/nav/buttonFindAnAlum.gif" alt="Find an Alum"></a>
+						<a href="/tools/mail?level=12&amp;groupname=saa&amp;pgOrg=saa&amp;dbc=y" target="_blank"><img src="/content/saa/homepage/nav/buttonCheckEmail.gif" alt="Check Email"></a>
+					</div>
+					
+					<form action="/get/page/search/results">
+					<div>
+					<input placeholder="Enter search item" name="keywords" id="keywords" class="text" onfocus="this.select();" style="border:1px solid #CCC; font-family: 'Lato', sans-serif; font-weight: 300;" type="text">
+				
+					<select id="searchType" name="section" class="StyledSelectReplaced" style="display: none;">
+					
+					<option value="discussion">Discussions</option>
+					<option value="group">Groups</option>
+					<option value="trip">Trips</option>
+					<option value="magazine">Magazine</option>
+					<option value="event">Events</option>
+					</select>
+					<div class="StyledSelectContainer"><div class="StyledSelect StyledSelectReplaced" style="width: 114px;"><div class="label"><div class="icon"><span>Entire Site</span></div></div><div class="ulcontainer" style="width: 112px; display: none;"><ul><li class="hover selected"><a href="javascript:void(0);" id="Entire_Site">Entire Site</a></li><li><a href="javascript:void(0);" id="Discussions">Discussions</a></li><li><a href="javascript:void(0);" id="Groups">Groups</a></li><li><a href="javascript:void(0);" id="Trips">Trips</a></li><li><a href="javascript:void(0);" id="Magazine">Magazine</a></li><li><a href="javascript:void(0);" id="Events">Events</a></li></ul></div></div></div>
+					<input class="image" value="Search" name="submit" src="/content/saa/homepage/nav/buttonSearch.gif" style="padding-left:5px;" type="image">
+
+					</div>
+					</form>
+					
+					<script type="text/javascript">
+					var searchTypeSS = new YAHOO.SolutionSet.widget.StyledSelect(document.getElementById('searchType'));
+					</script>
+ -->
+					
+					<!--div#header div#tools div.StyledSelect div.label { border: 1px solid #CCC; }	//-->
+					
+
+				</div>
+				<br>
+				<br>
+				<a href="/get/page/home" id="saaLogo">D.M.School</a>
+				<img src="img/campus1.jpg" id="masthead" style="min-width:946px;">
+			</div>
+		    
+		</div>
+		<!-- <div class="container" style="width: 1000px;padding-right: 0;padding-left: 0;"> -->
+			
+					<nav class="navbar navbar-default text-center" role="navigation">
+					  <div class="container-fluid">
+					    <!-- Brand and toggle get grouped for better mobile display -->
+					    <div class="navbar-header">
+					      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					      </button>
+					      <!-- <a class="navbar-brand" href="#">Brand Logo</a> -->
+					    </div>
+
+					    <!-- Collect the nav links, forms, and other content for toggling -->
+					    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					      	<ul class="nav navbar-nav">
+						      	<li class=""><a href="#">Home</a></li>
+						        <li><a href="#">About Us</a></li>
+						        <li><a href="#">Benefits & Services</a></li>
+						        <li><a href="#">News & Views</a></li>
+						        <li><a href="#">Events</a></li>
+						        <li><a href="#">Awards</a></li>
+						        <!-- <li class=""><a href="#">Dashboard</a></li>
+						        <li class=""><a href="#">Stories</a></li>
+						        <li><a href="#">Videos</a></li>
+						        <li><a href="#">Photos</a></li>	
+						       
+						        
+						        <li class="dropdown">
+						            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 </a>
+						            <ul class="dropdown-menu">
+						              <li><a href="#">Page 1-1</a></li>
+						              <li><a href="#">Page 1-2</a></li>
+						              <li><a href="#">Page 1-3</a></li>
+						            </ul>
+						        </li>
+						        <li><a href="#">Page 2</a></li> -->
+					      	</ul>
+					    </div><!-- /.navbar-collapse -->
+					  </div><!-- /.container-fluid -->
+					</nav>
+		
+
+		
+					
+					<div class="row" style="width: 976px;background: none;margin: 0 auto;">
+						<div class="row" >
+						   	<div class="col-sm-12 " style="background-color: #F8F6EF; padding:30px 1px 10px 1px;">
+						   	 	<div class="col-sm-6 col-md-6 col-lg-6">
+									<img src="img/3-IMG_23601.jpg" class="img-rounded" width="444" height="250"> 
+								</div>      
+								<div class="col-sm-6 col-md-6 col-lg-6">
+								<div style="display:block; line-height: 33px; font-family: 'Lato', sans-serif; font-size:35px; font-weight: 300; color: #454545;">
+									Get more summer out of your summer
+								</div>
+								<div style="font-size: 22px; padding-top:20px; display:block; font-family: 'Lato', sans-serif; font-weight: 300; color: #454545;">August is upon us. Get the most out of the dog days with a little help from I-Lab&nbsp;Alumni. </div>
+				  				<a href="http://alu.ms/2sujjrf" class="saabtn">Read</a><br><br>
+				  				</div>
+				    		</div>
+						</div>
+						<br>
+						<h2 style="color:#8C1515">OUR MAGAZINE</h2>
+						<div class="row">						  
+					        <div class="col-md-4 col-sm-6">
+					            <div class="our-team">
+					                <img src="img/beach1x.jpg" alt="">
+					                <div class="team-content">
+					                    <h3 class="team-title"><a href="#">Magic in the making</a><small>About Campus</small></h3>
+					                    <p class="description">Heads plus hands equals wizardry in Engineering’s Product Realization Lab</p>
+					                    <!-- <ul class="social-link">
+					                        <li><a href="#" class="fa fa-facebook"></a></li>
+					                        <li><a href="#" class="fa fa-google"></a></li>
+					                        <li><a href="#" class="fa fa-twitter"></a></li>
+					                        <li><a href="#" class="fa fa-pinterest"></a></li>
+					                    </ul> -->
+					                    <a href="#" class="read">Read <i class="fa fa-angle-right"></i></a>
+					                </div>
+					            </div>
+					        </div>
+					         <div class="col-md-4 col-sm-6">
+					            <div class="our-team">
+					                <img src="img/Homegoing1x.jpg" alt="">
+					                <div class="team-content">
+					                    <h3 class="team-title"><a href="#">The story behind ‘Homegoing’</a><small>Research</small></h3>
+					                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur dolorum!</p>
+					                    <!-- <ul class="social-link">
+					                        <li><a href="#" class="fa fa-facebook"></a></li>
+					                        <li><a href="#" class="fa fa-google"></a></li>
+					                        <li><a href="#" class="fa fa-twitter"></a></li>
+					                        <li><a href="#" class="fa fa-pinterest"></a></li>
+					                    </ul> -->
+					                    <a href="#" class="read">Read <i class="fa fa-angle-right"></i></a>
+					                </div>
+					            </div>
+					        </div>
+					         <div class="col-md-4 col-sm-6">
+					            <div class="our-team">
+					                <img src="img/ChangeMinds_1x.jpg" alt="">
+					                <div class="team-content">
+					                    <h3 class="team-title"><a href="#">'To change people's minds'</a><small>Activist</small></h3>
+					                    <p class="description">Activist Ayaan Hirsi Ali sees a better way to fight terrorism.</p>
+					                    <!-- <ul class="social-link">
+					                        <li><a href="#" class="fa fa-facebook"></a></li>
+					                        <li><a href="#" class="fa fa-google"></a></li>
+					                        <li><a href="#" class="fa fa-twitter"></a></li>
+					                        <li><a href="#" class="fa fa-pinterest"></a></li>
+					                    </ul> -->
+					                    <a href="#" class="read">Read <i class="fa fa-angle-right"></i></a>
+					                </div>
+					            </div>
+					        </div>
+    
+						    <!-- <div class="col-sm-4"> 
+						      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+						      <p>Project 2</p>    
+						    </div>
+						    <div class="col-sm-4">
+						      <div class="well">
+						       <p>Some text..</p>
+						      </div>
+						      <div class="well">
+						       <p>Some text..</p>
+						      </div>
+						    </div> -->
+						</div>	
+						<br>
+						<hr>
+						
+						<h2 style="color:#8C1515">CATCH A GLIMPSE</h2>
+						<div class="row">
+  							<div class="col-sm-8">	
+  								<iframe src="https://www.youtube.com/embed/94tFVX67bIA?rel=0&amp;showinfo=0" allowfullscreen="" width="570" height="321" frameborder="0"></iframe>
+  								<div class="section_title">I-lab Alumni. Never stop growing.</div>
+  								<div class="descrip">Wherever life takes you after i-lab, we're here to help.</div>
+  								<a href="#" class="saabtn"><span></span>Watch</a>
+  							</div>
+  							<div class="col-sm-4">
+  								<div class="row3_col2">
+									<div class="section_header" style="margin-bottom: 20px;">MY DASHBOARD</div>
+									<a href="#" class="dashboard_row">
+									<img src="img/1.gif"> Check my messages</a>
+									<hr>
+									<a href="#" class="dashboard_row">
+									<img src="img/2.gif"> Update my information</a>
+									<hr>
+									<a href="#" class="dashboard_row">
+									<img src="img/3.gif"> Join or keep up with groups</a>
+									<hr>
+									<a href="#" class="dashboard_row">
+									<img src="img/4.gif">Search the Alumni Directory</a>
+									<a href="#" class="saabtn" style="margin-top:40px; width: 131px;"> My I-Lab</a>
+								</div>
+
+  							</div>
+  						</div>
+  						<br>
+  						<hr>
+  						<div class="row">
+  							<div id="row6">
+								<div class="row6_col1">
+									<div class="section_header" style="color:#8C1515">LAST WORD</div>
+									
+									<div class="descrip">
+								 <img src="img/4.jpeg" class="img-rounded" width="255" height="255">
+								  <!-- <img src="/content/saa/homepage/images/Applebaum_2x.jpg"> --><div class="section_title2">Radical creativity</div>
+									  <p>Tune in for a funny peek into the mind of brilliant musician Mark Applebaum.</p>
+									</div>
+									<a href="#" class="saabtn"><span>Watch video of Seasons of Love</span>Watch</a>
+								</div>
+							  <div class="row6_col2">
+								<div class="section_header">SPONSORED</div>
+						<!-- <img src="/content/saa/homepage/images/stanford_outloud.gif"></a>
+						          -->
+						           
+						        <a href="#"><img src="img/Custom1.jpg" class="img-rounded" width="305" height="205"></a>
+
+						        </div>
+							</div>
+  						</div>
+  						<div class="row">
+  							<div id="new_footer">
+							
+		<div style="display: block;">
+			 <div class="footer_col">Community
+				 <a href="#">Classes, Groups &amp; Clubs</a>
+				 <a href="#">Alumni Directory</a>
+				 <a href="#">Discussions</a>
+				 <a href="#">Book Salon</a>
+				 <a href="#">Undergrad Students</a>
+				 <a href="#">Grad Students</a>
+				 <a href="#">Young Alumni</a>
+			 </div>
+			 <div class="footer_col">Resources
+			 	<a href="#">Alumni Career Services</a>
+			 	<a href="#">Perks</a>
+			 	<a href="#">Membership</a>
+			 	<a href="#">Your Alumni Center</a>
+			 	<a href="#">Alumni Center Events</a>
+			 </div>
+			 <div class="footer_col">News &amp; Views
+				 <a href="#">Magazine</a>
+				 <a href="#">Learn</a>
+				 <a href="#">Manage Subscriptions</a>
+				 <a href="#">Stanford News Links</a>
+			 </div>
+			 <div class="footer_col">Activities
+			 	<a href="#">My Class Reunion</a>
+			 	<a href="#">Events</a>
+			 	<a href="#">Where I Live</a>
+			 	<a href="#">Travel/Study</a>
+			 	<a href="#">Sierra Camp &amp; Programs</a>
+			 	<a href="#">On Campus</a>
+			 	<br><br>
+			 </div>
+			 <div class="footer_col">Volunteering
+				 <a href="#">Recognition</a>
+				 <a href="#">Leadership</a>
+				 <a href="#">Beyond the Farm</a>
+				 <a href="#">Trustee Nominations</a>
+			 </div>
+		</div>
+		<div style="display: block; padding-bottom: 10px;">
+			 <div class="footer_col">About
+			 	<a href="#">About I-lab</a>
+			 	<a href="#">Help</a>
+			 	<a href="#">Accessibility</a>
+			 	<a href="#">Privacy Policy</a>
+			 	<a href="#">Terms of Use</a>
+			 	<a href="#">Contact Us</a>
+			 </div>
+			 <div class="footer_col">My Account
+			 	<a href="#">Dashboard</a>
+			 	<a href="#">Profile</a>
+			 	<a href="#">Messages</a>
+			 	<a href="#">Friends</a>
+			 	<a href="#">Events</a>
+			 	<a href="#">Groups</a>
+			 	<a href="#">Gallery</a>
+			 </div>
+			 <div class="footer_col">University Links
+			 	<a href="#">Transcripts</a>
+			 	<a href="#">OVAL</a>
+			 	<a href="#">Giving to Stanford</a>
+			 	<a href="#">Visitor Information</a>
+			 	<a href="#">Stanford Athletics</a>
+			 	<a href="#">Stanford Homepage</a>
+			 </div>
+			 <div class="footer_col">Schools
+			 	<a href="#">Business</a>
+			 	<a href="#">Earth, Energy &amp; Environment</a>
+			 	<a href="#">Education</a>
+			 	<a href="#">Engineering</a>
+			 	<a href="#">Humanities &amp; Sciences</a>
+			 	<a href="#">Law</a>
+			 	<a href="#">Medicine</a><br><br>
+		</div>
+		<div id="footer_logos">
+		<a href="#"><img src="img/facebook.gif"></a>
+		<a href="#"><img src="img/twitter.gif"></a>
+		<a href="#"><img src="img/instagram.gif"></a>
+		<a href="#"><img src="img/linkedin.gif"></a>
+		<!-- <img src="/content/saa/homepage/images/saa_block.gif" style="margin:0 0 20px 0; float:right;"> -->
+		</div>
+
+							</div><!-- end id=new_footer -->
+					</div>
+  						</div>	
+
+					</div>
+			</div>
+					
+				
+				
+
+</body>
+</html> 
